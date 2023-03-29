5a6e9afff5bf7658980e8e95681b3c0064093772
zhfei <1174977961@qq.com>
2023-03-29 23:22:15 +0800

    feat: 完成数据存储与累加

--- js/popup.js
+++ js/popup.js
@@ -1,7 +1,25 @@
 $(document).ready(function(){
-  var title = $("#title").text();
-  $("#msg").keyup(function () {
+  var total = $("#total").text();
+  chrome.storage.sync.get('total',function (res) {
+    $("#total").text(res.total);
+    total = res.total;
+  })
+  
+  $('#add').click(function () {
     var msg = $("#msg").val();
-    $("#title").text(title+":"+msg);
+    var totalCount = 0;
+    if (msg && total) {
+      totalCount += parseFloat(msg);
+      totalCount += parseFloat(total);
+    } else if (msg) {
+      totalCount += parseFloat(msg);
+    }
+
+    chrome.storage.sync.set({'total': totalCount});
+
+    $("#total").text(totalCount);
+    $("#msg").val('');
+
+    console.log(totalCount)
   })
 });
